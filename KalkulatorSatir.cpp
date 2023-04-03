c9afc5f140ad5667dfff770925ba9e321089af99
JuliAstaa <128821113+JuliAstaa@users.noreply.github.com>
2023-04-03 10:19:41 +0700

    Update KalkulatorSatir.cpp

--- KalkulatorSatir.cpp
+++ KalkulatorSatir.cpp
@@ -1,88 +1,95 @@
 #include <iostream>
 using namespace std;
 
-float Penjumlahan(float angka1, float angka2){
+//procedure
+void Penjumlahan(float angka1, float angka2){
 	float hasil = angka1 + angka2;
-	return hasil;
+	cout<<"Hasil dari "<<angka1<<" + "<<angka2<<" = "<<hasil<<endl;
 }
 
-float Pengurangan(float angka1, float angka2){
+void Pengurangan(float angka1, float angka2){
 	float hasil = angka1 - angka2;
-	return hasil;
+	cout<<"Hasil dari "<<angka1<<" - "<<angka2<<" = "<<hasil<<endl;
 }
 
-float Perkalian(float angka1, float angka2){
+void Perkalian(float angka1, float angka2){
 	float hasil = angka1 * angka2;
-	return hasil;
+	cout<<"Hasil dari "<<angka1<<" x "<<angka2<<" = "<<hasil<<endl;
 }
 
-float Pembagian(float angka1, float angka2){
+void Pembagian(float angka1, float angka2){
 	float hasil = angka1 / angka2;
-	return hasil;
+	cout<<"Hasil dari "<<angka1<<" : "<<angka2<<" = "<<hasil<<endl;
 }
+//end procedure
 
+//main
 int main(){
-	
-	int  ulang = 0;
-	
-	while(ulang == 0){
-		cout<<"========================\n";
-		cout<<"Program Kalkulator Satir\n";
-		cout<<"------------------------\n";
+	int ulang = 0;
+	while(ulang == 0){	
+		cout<<"==========================="<<endl;
+		cout<<"         Kalkulator        "<<endl;
+		cout<<"---------------------------"<<endl;
 		
-		cout<<"1. Penjumlahan\n";
-		cout<<"2. Pengurangan\n";
-		cout<<"3. Perkalian\n";
-		cout<<"4. Pembagian\n";
-		
-		string Operator;
+		//variabel
+		string pilih;
 		int pilihan;
 		float angka1, angka2, hasil;
-		cout<<"------------------------\n";
-		cout<<"Silahkan masukkan pilihan: ";
-		cin>>pilihan;
-		
-		cout<<"Masukkan angka 1: ";
+		//end variabel
+		cout<<"Masukkan angka pertama : ";
 		cin>>angka1;
-		cout<<"Masukkan angka 2: ";
+		cout<<"Masukkan angka kedua   : ";
 		cin>>angka2;
 		
-			switch (pilihan){
-				case 1:
-					hasil = Penjumlahan(angka1, angka2);
-					Operator = "+";
-					break;
-				case 2:
-					hasil = Pengurangan(angka1, angka2);
-					Operator = "-";
-					break;
-				case 3:
-					hasil = Perkalian(angka1, angka2);
-					Operator = "x";
-					break;
-				case 4:
-					hasil = Pembagian(angka1, angka2);
-					Operator = ":";
-					break;
-				default:
-					cout<<"Masukkan sesuai dengan pilihan yang tersedia";
-					break;
-					
-			}
-		cout<<"------------------------\n";
+		cout<<endl;
+		cout<<"1. Penjumlahan(+)"<<endl;
+		cout<<"2. Pengurangan(-)"<<endl;
+		cout<<"3. Perkalian  (x)"<<endl;
+		cout<<"4. Pembagian  (:)"<<endl;
+		cout<<"Pilih operator : ";
+		cin>>pilihan;
+		cout<<endl;
 		
-		if(Operator == ""){
-			cout<<"Sytem Error\n";
-		} else {
-			cout<<"Hasil dari "<<angka1<<" "<<Operator<<" "<<angka2<<" = "<<hasil<<"\n";
+		//percabangan
+		switch(pilihan){
+			case 1:
+				Penjumlahan(angka1, angka2);
+				pilih = "+";
+				break;
+			case 2:
+				Pengurangan(angka1, angka2);
+				pilih = "-";
+				break;
+			case 3:
+				Perkalian(angka1, angka2);
+				pilih = "x";
+				break;
+			case 4:
+				Pembagian(angka1, angka2);
+				pilih = ":";
+				break;
+			default:
+				cout<<"Pilih operator yang sudah disediakan"<<endl;
+				break;
 		}
+		// end percabangan
 		
+		//Jika user tidak memilih operator sesuai dengan pilihan maka akan cout system error
+		if(pilih == ""){
+			cout<<"System error"<<endl;
+		}
+		//end if
 		
-		cout<<"Tekan 0 untuk ulang, atau tekan 1 untuk keluar: ";
+		//cinn akan mengirim angka ke variabel ulang yang berada di atas:
+		//Jika user mengetik 0, kalkukator akan diulang, jika selain angka 0 maka program akan keluar while
+		//jika keluar while maka program akan me return nilai 0, dan program akan berhenti
+		cout<<"Tekan 0 untuk ulang, tekan 1 untuk keluar ";
 		cin>>ulang;
-	}	
-		return 0;
+		cout<<endl;
+	}
+	
+	cout<<"Terimakasih"<<endl;
+	return 0;
 	
 	
-
 }
